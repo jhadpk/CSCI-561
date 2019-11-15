94b8bf41e4b5a4e4baa43bfeef8cd4fd88fdceae
Deepak Jha <deepakjha@Deepaks-MBP.lan1>
2019-11-14 23:37:24 -0800

    final commit before submit

--- assignment-2/src/main/java/HeuristicValues.java
+++ assignment-2/src/main/java/HeuristicValues.java
@@ -7,12 +7,9 @@ public class HeuristicValues {
     public static final int MOVING_INSIDE_OPPOSITION_CAMP               = 10;
     public static final int MOVING_INTO_OPPOSITION_CAMP_FROM_OUTSIDE    = 20;
 
-    public static final int FUTURE_JUMP_MOVE_BONUS                      = 10;
-    public static final int JUMP_MOVE_BONUS                             = 5;
     public static final int VERTICAL_MOVE_BONUS                         = 5;
     public static final int DIAGONAL_MOVE_BONUS                         = 10;
 
-    //public static final int BOARD_WITH_CELL_IN_DIAGONAL_PATH            = 10;
     public static final int CELL_IN_DIAGONAL_PATH_BONUS                 = 30;
     public static final int CELL_IN_OPPOSITION_CAMP_BONUS               = 40;
 }
